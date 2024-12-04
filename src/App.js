a4cd6f78537ccc5ec74f4a8888a258022f4b3cf5
SrivarshiniSG <srivarshini.sg29@gmail.com>
2024-12-04 13:36:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,51 @@
+// import React from "react";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import Home from "./routes/Home";
+// import About from "./routes/About";
+// import Team from "./routes/Team";
+// import Events from "./routes/Events";
+// import CodersForum from "./routes/CodersForum";
+// import Fored from "./routes/Fored";
+// import Contact from "./routes/Contact";
+// import Submitresume from "./routes/Submitresume";
+// import MockPlacement from "./routes/MockPlacement";
+// import Mock from "./routes/Mock";
+// import Aptitude from "./routes/Aptitude";
+// import GroupDiscussion from "./routes/GroupDiscussion";
+// import Seminar from "./routes/Seminar";
+// import wwdcard from "./routes/wwdcard";
+
+// function App() {
+//   return (
+//        <Routes>
+//         <Route path="/Forese-Website-Trial1">
+//           <Route path="/Forese-Website-Trial1/home" element = {<Home />} />
+//           <Route path="/Forese-Website-Trial1/about" element = {<About />} />
+//           <Route path="/Forese-Website-Trial1/team" element = {<Team />} />
+//           <Route path="/Forese-Website-Trial1/events" element = {<Events />} />
+//           <Route path="/Forese-Website-Trial1/codersforum" element = {<CodersForum />} />
+//           {/* <Route path="/mockplacements" element={<MockPlacements />}/> */}
+//           <Route path="/Forese-Website-Trial1/mockplacement" element = {<MockPlacement />} />
+//           <Route path="/Forese-Website-Trial1/fored" element = {<Fored />} />
+//           <Route path="/Forese-Website-Trial1/contact" element = {<Contact />} />
+//           <Route path="/Forese-Website-Trial1/submitresume" element = {<Submitresume />}/>
+//           <Route path="/Forese-Website-Trial1/mock" element = {<Mock/>}/>
+//           <Route path="/Forese-Website-Trial1/aptitude" element = {<Aptitude/>}/>
+//           <Route path="/Forese-Website-Trial1/groupdiscussion" element = {<GroupDiscussion/>}/>
+//           {/* <Route path="/seminar" element = {<Seminar/>}/> */}
+//           <Route path="/Forese-Website-Trial1/seminar" element = {<Seminar/>}/>
+//           <Route element = {<wwdcard/>}/>
+//         </Route>
+//         </Routes>
+     
+//   );
+// }
+
+// export default App;
+
+
+
+
 import React from "react";
 import "./index.css";
 import Home from "./routes/Home"
@@ -20,12 +68,16 @@ import wwdcard from "./routes/wwdcard";
 
 
 
-import { Route, Routes } from "react-router-dom";
+// import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
     <div>
       <>
+      {/* <Router basename="/Forese-Website-Trial1"> */}
+      {/* <Router> */}
         <Routes>
           <Route path="/" element = {<Home />} />
           <Route path="/about" element = {<About />} />
@@ -44,6 +96,7 @@ function App() {
           <Route path="/seminar" element = {<Seminar/>}/>
           <Route element = {<wwdcard/>}/>
         </Routes>
+      {/* </Router> */}
       </>
     </div>
   );
